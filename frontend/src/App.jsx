21ba9cefa23f01a8bb39d30b685cbd08dc804849
Pawankul19 <kulshrestha19032721@gmail.com>
2023-12-08 17:48:49 +0530

    navbar icon updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Navbar } from './Components/Navbar'
+import { Navbar } from "./Components/Navbar";
 import { List } from './Components/list'
 
 function App() {
