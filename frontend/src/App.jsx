476369e48d5281810cca20519da0b451a35598ec
ashish garg <ashish.garg@metamerch.in>
2023-12-08 19:03:52 +0530

    make chat list area

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './styles/listarea.css'
 import { Navbar } from "./Components/Navbar";
 import { List } from './Components/list'
 import { Chat  } from "./Components/Chat";
--- frontend/src/Components/List.jsx
+++ frontend/src/Components/List.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { CiSearch } from "react-icons/ci";
+
+
 
 export const List = () => {
   return (
-    <div className='ListArea'>list</div>
+    <div className='ListArea'>
+      <h2 className='ml-6 headingfont'>Message</h2>
+      <div className='searchbar is-flex is-align-items-center'>
+        <CiSearch color='black' size={20} />
+        <input placeholder='Search' />
+      </div>
+    </div>
   )
 }
