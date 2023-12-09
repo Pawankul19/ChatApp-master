842dbb006f19f75eb2d789777004295518c02309
Pawankul19 <kulshrestha19032721@gmail.com>
2023-12-09 13:12:21 +0530

    profile map successfully

--- frontend/src/Components/List.jsx
+++ frontend/src/Components/List.jsx
@@ -16,12 +16,12 @@ export const List = () => {
         "profile_picture": "https://api.slingacademy.com/public/sample-users/1.png",
         "first_name": "Kayla",
         "email": "kayla.lopez.1@slingacademy.com",
-        "last_name": "Lopez",
+        "last_message": "hii",
         "phone": "+1-697-415-3345x5215",
         "street": "3388 Roger Wells Apt. 010",
         "state": "Vermont",
         "country": "Jordan",
-        "longitude": 112.16014
+        "time": "10:01AM"
       },
       {
         "id": 2,
@@ -34,12 +34,12 @@ export const List = () => {
         "profile_picture": "https://api.slingacademy.com/public/sample-users/2.png",
         "first_name": "Tina",
         "email": "tina.patrick.2@slingacademy.com",
-        "last_name": "Patrick",
+        "last_message": "hello",
         "phone": "800-865-4932",
         "street": "4564 Gamble Light Suite 885",
         "state": "Kansas",
         "country": "Greenland",
-        "longitude": -21.22766
+        "time": "10:03AM"
       },
       {
         "id": 3,
@@ -52,12 +52,12 @@ export const List = () => {
         "profile_picture": "https://api.slingacademy.com/public/sample-users/3.png",
         "first_name": "Brittany",
         "email": "brittany.bradford.3@slingacademy.com",
-        "last_name": "Bradford",
+        "last_message": "Happy Birthday",
         "phone": "205-439-1326",
         "street": "0193 Amy Isle",
         "state": "Indiana",
         "country": "Micronesia",
-        "longitude": 147.58546
+        "time": "10:05AM"
       },
       {
         "id": 4,
@@ -70,19 +70,87 @@ export const List = () => {
         "profile_picture": "https://api.slingacademy.com/public/sample-users/4.png",
         "first_name": "Lisa",
         "email": "lisa.thomas.4@slingacademy.com",
-        "last_name": "Thomas",
+        "last_message": "Oyo chalo",
         "phone": "(660)262-7257",
         "street": "3500 Miller Springs Suite 728",
         "state": "Connecticut",
         "country": "Sudan",
-        "longitude": -88.334238
+        "time": "10:06AM"
+      },
+      {
+        "id": 1,
+        "gender": "female",
+        "date_of_birth": "2002-04-26T00:00:00",
+        "job": "Herbalist",
+        "city": "Humphreyfurt",
+        "zipcode": "79574",
+        "latitude": 13.032103,
+        "profile_picture": "https://api.slingacademy.com/public/sample-users/1.png",
+        "first_name": "Kayla",
+        "email": "kayla.lopez.1@slingacademy.com",
+        "last_message": "hii",
+        "phone": "+1-697-415-3345x5215",
+        "street": "3388 Roger Wells Apt. 010",
+        "state": "Vermont",
+        "country": "Jordan",
+        "time": "10:01AM"
+      },
+      {
+        "id": 1,
+        "gender": "female",
+        "date_of_birth": "2002-04-26T00:00:00",
+        "job": "Herbalist",
+        "city": "Humphreyfurt",
+        "zipcode": "79574",
+        "latitude": 13.032103,
+        "profile_picture": "https://api.slingacademy.com/public/sample-users/1.png",
+        "first_name": "Kayla",
+        "email": "kayla.lopez.1@slingacademy.com",
+        "last_message": "hii",
+        "phone": "+1-697-415-3345x5215",
+        "street": "3388 Roger Wells Apt. 010",
+        "state": "Vermont",
+        "country": "Jordan",
+        "time": "10:01AM"
+      },
+      {
+        "id": 1,
+        "gender": "female",
+        "date_of_birth": "2002-04-26T00:00:00",
+        "job": "Herbalist",
+        "city": "Humphreyfurt",
+        "zipcode": "79574",
+        "latitude": 13.032103,
+        "profile_picture": "https://api.slingacademy.com/public/sample-users/1.png",
+        "first_name": "Kayla",
+        "email": "kayla.lopez.1@slingacademy.com",
+        "last_message": "hii",
+        "phone": "+1-697-415-3345x5215",
+        "street": "3388 Roger Wells Apt. 010",
+        "state": "Vermont",
+        "country": "Jordan",
+        "time": "10:01AM"
+      },
+      {
+        "id": 1,
+        "gender": "female",
+        "date_of_birth": "2002-04-26T00:00:00",
+        "job": "Herbalist",
+        "city": "Humphreyfurt",
+        "zipcode": "79574",
+        "latitude": 13.032103,
+        "profile_picture": "https://api.slingacademy.com/public/sample-users/1.png",
+        "first_name": "Kayla",
+        "email": "kayla.lopez.1@slingacademy.com",
+        "last_message": "hii",
+        "phone": "+1-697-415-3345x5215",
+        "street": "3388 Roger Wells Apt. 010",
+        "state": "Vermont",
+        "country": "Jordan",
+        "time": "10:01AM"
       }
     ]
   )
-
-
-
-
 
   return (
     <div className='ListArea'>
@@ -91,7 +159,14 @@ export const List = () => {
         <CiSearch color='black' size={20} />
         <input placeholder='Search' />
       </div>
-      <ListItems username={"items.firstName"} image={""} last_message={"items.lastName"} />
+      <div className='profile'>
+      {data.map((num)=>{
+        console.log(num.first_name)
+        return (
+      <ListItems username={num.first_name} image={num.profile_picture} last_message={num.last_message}  time={num.time} />
+        )
+      })}
+      </div>
     </div>
   );
 }
